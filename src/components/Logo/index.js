eadb2d763b26335cc5d967c3418976829f02d423
Felipe Alves <felipeaamodio@gmail.com>
2021-07-20 16:00:08 -0300

    first commit

---
+++ src/components/Logo/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, StyleSheet, Platform} from 'react-native';
+
+export default function Logo(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.bank}>Bank</Text>
+            <Text style={styles.four}>4</Text>
+            <Text style={styles.devs}>Devs</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginBottom: 30
+    },
+    bank: {
+        fontSize: 27,
+        fontWeight: '600',
+        color: '#FFFFFF'
+    },
+    four: {
+        fontSize: 31,
+        fontWeight: 'bold'
+    },
+    devs: {
+        fontSize: 27,
+        fontWeight: '600',
+        color: '#FFFFFF'
+    }
+})
